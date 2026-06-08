f2ddb99159946ae16687508316ce76496869374f
Emilio Flores <emiliofc99@outlook.com>
2026-06-07 18:19:58 -0600

    refactor(iac): strongly type module variables and enforce file separation standards

--- infrastructure/modules/cloud-storage/main.tf
+++ infrastructure/modules/cloud-storage/main.tf
@@ -1,9 +1,9 @@
-variable "environment" {}
-variable "project_id" {}
-
 resource "google_storage_bucket" "raw_zone" {
   name          = "reverse-etl-raw-${var.environment}"
   location      = "us-central1"
+  project       = var.project_id
+  
+  # Allow Terraform to easily destroy the dev bucket, but protect the prod bucket
   force_destroy = var.environment == "dev" ? true : false
   
   uniform_bucket_level_access = true
